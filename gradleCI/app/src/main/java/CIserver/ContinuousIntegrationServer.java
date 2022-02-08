e74db076c610c2e7f2f25e21a61ab29c714fe53a
Agnes <agnesfo@kth.se>
2022-02-08 12:02:45 +0100

    feat #2 part of solution, clones repo and enters branch

--- gradleCI/app/src/main/java/CIserver/ContinuousIntegrationServer.java
+++ gradleCI/app/src/main/java/CIserver/ContinuousIntegrationServer.java
@@ -3,9 +3,11 @@ package CIserver;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
+import java.io.BufferedReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -33,8 +35,76 @@ public class ContinuousIntegrationServer extends AbstractHandler
         // 1st clone your repository
         // 2nd compile the code
 
+        if(baseRequest.getMethod().equals("POST")){
+            String body = getBody(baseRequest);
+            String repo = null;
+            String branch = null;
+
+            for(String line : body.split("\n")){
+                if(line.contains("full_name")){
+                    repo = line.split(":")[1].split(",")[0];
+                    //TODO check if default_branch is correct
+                } else if(line.contains("default_branch")){
+                    branch = line.split(":")[1].split(",")[0];
+                }
+            }
+
+            if(repo != null && branch != null){
+                Runtime runtime = Runtime.getRuntime();
+                String cloneOutput = runCommand("git clone git@github.com:" + repo + " tempRepo", runtime);
+                String cdOutput = runCommand("cd tempRepo", runtime);
+                String branchOutput = runCommand("git checkout " + branch, runtime);
+
+
+
+                // Last: cleanup
+                runCommand("rm -r tempRepo", runtime);
+            }else{
+                // The POST request does not have the intended headers, something is wrong.
+            }
+        }else{
+            // This is not a Webhook, so not a request we want to handle.
+        }
+
         response.getWriter().println("CI job done");
     }
+
+
+    public String getBody(Request request) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } finally {
+            reader.close();
+        }
+
+        return sb.toString();
+    }
+
+    public String runCommand(String command, Runtime runtime) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            Process proc = runtime.exec(command);
+            // Read the output
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = "";
+            while((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+
+            proc.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+ 
  
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
