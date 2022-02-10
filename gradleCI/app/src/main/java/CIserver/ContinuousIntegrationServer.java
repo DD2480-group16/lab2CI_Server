4e1cb0e6e16ec7a9dcf0bfc6b3e92fae4ec93915
agnesforsberg <66953501+agnesforsberg@users.noreply.github.com>
2022-02-10 16:34:49 +0100

    #21 tests for server functions (#25)
    * feat #21 tests and documentation for helper functions, changed webhook output
    
    * fix remove broken test
    
    Co-authored-by: Agnes <agnesfo@kth.se>

--- gradleCI/app/src/main/java/CIserver/ContinuousIntegrationServer.java
+++ gradleCI/app/src/main/java/CIserver/ContinuousIntegrationServer.java
@@ -14,10 +14,7 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import java.io.PrintWriter;
 
-/** 
- Skeleton of a ContinuousIntegrationServer which acts as webhook
- See the Jetty documentation for API documentation of those classes.
-*/
+
 public class ContinuousIntegrationServer extends AbstractHandler
 {
     public void handle(String target,
@@ -30,15 +27,8 @@ public class ContinuousIntegrationServer extends AbstractHandler
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
-        //System.out.println(baseRequest.getMethod());
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-
         if(baseRequest.getMethod().equals("POST")){
-            String body = getBody(baseRequest);
+            String body = getBody(baseRequest.getReader());
             String repo = null;
             String branch = null;
 
@@ -46,14 +36,10 @@ public class ContinuousIntegrationServer extends AbstractHandler
             for(String line : body.split(",")){
                 if(line.contains("clone_url")){
                     repo = line.substring(13, line.length()-1);
-                    //TODO check if default_branch is correct
-                    System.out.println(repo);
                 } else if(line.contains("\"ref\":")){
                     branch = line.split(":")[1].split("/")[2];
                     branch = branch.substring(0, branch.length()-1);
-                    System.out.println(branch);
                 }
-
             }
 
             if(repo != null && branch != null){
@@ -63,7 +49,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
                 Runtime runtime = Runtime.getRuntime();
                 String currentDir = System.getProperty("user.dir");
-                System.out.println("current directory:\t" + currentDir.toString());
+                //System.out.println("current directory:\t" + currentDir.toString());
                 System.out.println("\nInitializing Build...");
                 String cloneOutput = runCommand("git clone " + repo + " tempRepo", runtime, new File(currentDir));
                 String branchOutput = "Branch is Main.";
@@ -71,10 +57,9 @@ public class ContinuousIntegrationServer extends AbstractHandler
                     branchOutput = runCommand("git checkout " + branch, runtime, new File(currentDir + "/tempRepo"));
                 }
 
-
-
                 PrintWriter writer = response.getWriter();
-                writer.print(cloneOutput + "\n" + branchOutput + "\n" + "Initializing build... (See e-mail for results.");
+                writer.print("Successfully cloned repo." + "\n" + "Now in branch " + branch + "\n" 
+                    + "Initializing build... (See e-mail for results.");
 
                 String buildOutput = runCommand("./gradlew build", runtime, new File(currentDir+"/tempRepo/gradleCI"));
                 //String testOutput = runCommand("./gradlew test", runtime, new File(currentDir+"/tempRepo/gradleCI"));
@@ -90,13 +75,11 @@ public class ContinuousIntegrationServer extends AbstractHandler
                 System.out.print(buildOutput);
                 System.out.println("------------------------------------------------------------\n");
 
-                // Last: cleanup
+                // Cleanup
                 runCommand("rm -r tempRepo", runtime, new File(currentDir));
 
-
-
             }else{
-                // The POST request does not have the intended headers, something is wrong.
+                // The POST request body does not have the intended headers, something is wrong.
                 System.out.println("\n RECIEVED MALFORMED POST REQUEST. (Discarded)\n");
                 response.getWriter().println("You do not have the intended headers, something is wrong.");
             }
@@ -107,9 +90,14 @@ public class ContinuousIntegrationServer extends AbstractHandler
         }
     }
 
-    public String getBody(Request request) throws IOException{
+    /** Reads a body from a BufferedReader and returns the String representation.
+     * 
+     * @param reader The reader holding the body
+     * @return String representation of body, without newlines.
+     * @throws IOException
+     */
+    public String getBody(BufferedReader reader) throws IOException{
         StringBuilder sb = new StringBuilder();
-        BufferedReader reader = request.getReader();
         try {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -122,8 +110,16 @@ public class ContinuousIntegrationServer extends AbstractHandler
         return sb.toString();
     }
 
+    /** Runs a command and returns the output, error or not.
+     *  
+     * @param command The command to run
+     * @param runtime The runtime that the command is run in
+     * @param dir   The directory in which the command is run
+     * @return The string representation of the command output.
+     */
     public String runCommand(String command, Runtime runtime, File dir) {
         StringBuilder sb = new StringBuilder();
+        System.out.println("hejsan");
         try {
             Process proc = runtime.exec(command, null, dir);
             // Read the output
---
+++ gradleCI/app/src/test/java/CIserver/CIServerTest.java
@@ -0,0 +1,31 @@
+package CIserver;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CIServerTest {
+    
+    @Test
+    public void getBodyReturnsStringOfReaderContent() throws FileNotFoundException, IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/src/test/java/CIserver/testFile.txt"));
+        ContinuousIntegrationServer ci = new ContinuousIntegrationServer();
+        String result = ci.getBody(reader);
+
+        assertEquals("{    1:this is line1,    2:this is line2}", result);
+    }
+
+    @Test
+    public void getBodyEmptyStringWhenNoContent()throws FileNotFoundException, IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/src/test/java/CIserver/testEmptyFile.txt"));
+        ContinuousIntegrationServer ci = new ContinuousIntegrationServer();
+        String result = ci.getBody(reader);
+
+        assertEquals("", result);
+    }
+}
